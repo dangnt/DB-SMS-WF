fc100211d6702ca4a07f67c8039ff3d6b98a4ea2
Nguyễn Tuấn Đăng <dangnt@sgu.edu.vn>
2022-06-25 15:07:12 +0700

    Add project files.

---
+++ DB-SMS-WF/StudentForm.Designer.cs
@@ -0,0 +1,264 @@
+﻿
+namespace DB_SMS_WF
+{
+    partial class StudentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.StudentListView = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.birthday = new System.Windows.Forms.DateTimePicker();
+            this.program = new System.Windows.Forms.TextBox();
+            this.name = new System.Windows.Forms.TextBox();
+            this.sid = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.DELETE = new System.Windows.Forms.Button();
+            this.EDIT = new System.Windows.Forms.Button();
+            this.ADD = new System.Windows.Forms.Button();
+            this.NEW = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.StudentListView)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.StudentListView);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 264);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "STUDENT LIST";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // StudentListView
+            // 
+            this.StudentListView.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.StudentListView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.StudentListView.Location = new System.Drawing.Point(6, 20);
+            this.StudentListView.Name = "StudentListView";
+            this.StudentListView.Size = new System.Drawing.Size(764, 225);
+            this.StudentListView.TabIndex = 0;
+            this.StudentListView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.StudentListView_CellContentClick);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.birthday);
+            this.groupBox2.Controls.Add(this.program);
+            this.groupBox2.Controls.Add(this.name);
+            this.groupBox2.Controls.Add(this.sid);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 295);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(630, 112);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "STUDENT DATA";
+            // 
+            // birthday
+            // 
+            this.birthday.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.birthday.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.birthday.Location = new System.Drawing.Point(493, 34);
+            this.birthday.Name = "birthday";
+            this.birthday.Size = new System.Drawing.Size(125, 26);
+            this.birthday.TabIndex = 7;
+            // 
+            // program
+            // 
+            this.program.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.program.Location = new System.Drawing.Point(495, 74);
+            this.program.Name = "program";
+            this.program.Size = new System.Drawing.Size(123, 26);
+            this.program.TabIndex = 6;
+            // 
+            // name
+            // 
+            this.name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.Location = new System.Drawing.Point(112, 75);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(244, 26);
+            this.name.TabIndex = 5;
+            // 
+            // sid
+            // 
+            this.sid.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sid.Location = new System.Drawing.Point(112, 39);
+            this.sid.Name = "sid";
+            this.sid.Size = new System.Drawing.Size(123, 26);
+            this.sid.TabIndex = 4;
+            this.sid.TextChanged += new System.EventHandler(this.sid_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(387, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "PROGRAM";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(387, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "BIRTHDAY";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(2, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "FULL NAME";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.DELETE);
+            this.groupBox3.Controls.Add(this.EDIT);
+            this.groupBox3.Controls.Add(this.ADD);
+            this.groupBox3.Location = new System.Drawing.Point(648, 295);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(133, 142);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "OPERATIONS";
+            // 
+            // DELETE
+            // 
+            this.DELETE.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DELETE.Location = new System.Drawing.Point(28, 108);
+            this.DELETE.Name = "DELETE";
+            this.DELETE.Size = new System.Drawing.Size(88, 28);
+            this.DELETE.TabIndex = 2;
+            this.DELETE.Text = "DELETE";
+            this.DELETE.UseVisualStyleBackColor = true;
+            this.DELETE.Click += new System.EventHandler(this.DELETE_Click);
+            // 
+            // EDIT
+            // 
+            this.EDIT.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EDIT.Location = new System.Drawing.Point(28, 74);
+            this.EDIT.Name = "EDIT";
+            this.EDIT.Size = new System.Drawing.Size(88, 25);
+            this.EDIT.TabIndex = 1;
+            this.EDIT.Text = "EDIT";
+            this.EDIT.UseVisualStyleBackColor = true;
+            this.EDIT.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ADD
+            // 
+            this.ADD.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ADD.Location = new System.Drawing.Point(28, 34);
+            this.ADD.Name = "ADD";
+            this.ADD.Size = new System.Drawing.Size(88, 26);
+            this.ADD.TabIndex = 0;
+            this.ADD.Text = "ADD";
+            this.ADD.UseVisualStyleBackColor = true;
+            this.ADD.Click += new System.EventHandler(this.ADD_Click);
+            // 
+            // NEW
+            // 
+            this.NEW.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NEW.Location = new System.Drawing.Point(341, 411);
+            this.NEW.Name = "NEW";
+            this.NEW.Size = new System.Drawing.Size(88, 26);
+            this.NEW.TabIndex = 3;
+            this.NEW.Text = "NEW";
+            this.NEW.UseVisualStyleBackColor = true;
+            this.NEW.Click += new System.EventHandler(this.NEW_Click);
+            // 
+            // StudentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.NEW);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "StudentForm";
+            this.Text = "STUDENT FORM";
+            this.Load += new System.EventHandler(this.StudentForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.StudentListView)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView StudentListView;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox program;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.TextBox sid;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker birthday;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button DELETE;
+        private System.Windows.Forms.Button EDIT;
+        private System.Windows.Forms.Button ADD;
+        private System.Windows.Forms.Button NEW;
+    }
+}
+
---
+++ DB-SMS-WF/StudentForm.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DB_SMS_WF
+{
+    public partial class StudentForm : Form
+    {
+        static string connectionString = @"Data Source = DESKTOP-FJV5Q13; Initial Catalog = SMS; Integrated Security = true";
+        static SqlConnection connection;
+        static SqlDataAdapter adapter = new SqlDataAdapter();
+        static SqlCommand command;
+
+        private void StudentListLoad()
+        {
+            string queryString = "SELECT * FROM Student";
+            StudentListView.DataSource = SelectRows(queryString);
+        }
+
+        private static DataTable SelectRows(string queryString)
+        {
+            DataTable table = new DataTable();
+
+            command = connection.CreateCommand();
+            command.CommandText = queryString;
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            return table;
+        }
+        public StudentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            command = connection.CreateCommand();
+            command.CommandText = "UPDATE Student SET name = '" + name.Text + "', birthday = '" + birthday.Text + "', program = '" + program.Text + "' WHERE sid = '" + sid.Text + "'";
+            command.ExecuteNonQuery();
+
+            string queryString = "SELECT * FROM Student";
+            StudentListView.DataSource = SelectRows(queryString);
+        }
+
+        private void StudentForm_Load(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            StudentListLoad();
+        }
+
+        private void StudentListView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i = StudentListView.CurrentRow.Index;
+
+            sid.Text = StudentListView.Rows[i].Cells[0].Value.ToString();
+            sid.ReadOnly = true;
+            name.Text = StudentListView.Rows[i].Cells[1].Value.ToString();
+            birthday.Text = StudentListView.Rows[i].Cells[2].Value.ToString();
+            program.Text = StudentListView.Rows[i].Cells[3].Value.ToString();
+        }
+
+        private void ADD_Click(object sender, EventArgs e)
+        {
+            command = connection.CreateCommand();
+            command.CommandText = "INSERT INTO Student VALUES ('" + sid.Text + "', '" + name.Text + "', '" + birthday.Text + "', '" + program.Text + "')";
+            command.ExecuteNonQuery();
+
+            StudentListLoad();
+        }
+
+        private void DELETE_Click(object sender, EventArgs e)
+        {
+            command = connection.CreateCommand();
+            command.CommandText = "DELETE FROM Student WHERE sid = '" + sid.Text + "'";
+            command.ExecuteNonQuery();
+
+            StudentListLoad();
+        }
+
+        private void sid_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NEW_Click(object sender, EventArgs e)
+        {
+            sid.Text = "";
+            sid.ReadOnly = false;
+            name.Text = "";
+            birthday.Text = "";
+            program.Text = "";
+        }
+    }
+}
